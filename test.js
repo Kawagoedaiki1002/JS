3f065a965d6a5bbb534852edf92ecf210a1f601a
Kawagoedaiki1002 <104619406+Kawagoedaiki1002@users.noreply.github.com>
2022-07-06 19:14:53 +0900

    追加

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ window.addEventListener('click', function(){
     const pullDownButton= document.getElementById("lists")
     console.log(pullDownButton)
     pullDownButton.addEventListener('mouseover',function(){
-        pullDownButton.setAttribute("style","background-color:green;")
+        pullDownButton.setAttribute("style","background-color:blue;")
     })
     pullDownButton.addEventListener('mouseout',function(){
         pullDownButton.removeAttribute("style","background-color:red;")
